6eda7a32f32aa1453bd99e2065f57c6c59d9052d
BoryMory <>
2026-05-22 12:37:14 +0200

    first flashattn test

--- src/03_flashAttn/01_flashAttn_naive/kernel.cu
+++ src/03_flashAttn/01_flashAttn_naive/kernel.cu
@@ -9,7 +9,7 @@ namespace FlashLab::flashAttn::naive {
     //
     // KERNELS
     //
-    template<const int Br, const int Bc>
+    template<const int Br, const int Bc, const int D>
     __global__ void flashAttn_fwd_v1 (float *K, float *Q, float *V, float *O, const int N, const int d) {
         // We launch Br * 32 threads. SMEM loading is tiled
         int rowIdx = blockIdx.x * Br;
@@ -19,73 +19,117 @@ namespace FlashLab::flashAttn::naive {
         int tx_i = threadIdx.x % 32;
         int ty_i = threadIdx.x / 32;
 
-        int tx_j = (threadIdx.x * Bc) % (Br * 32);
-        int ty_j = (threadIdx.x * Bc) / (Br * 32);
+        int tx_j = (threadIdx.x) % (32 * Br / Bc);
+        int ty_j = (threadIdx.x) / (32 * Br / Bc);
 
         __shared__ float Q_i [Br * d];
         __shared__ float K_j [Bc * d];
         __shared__ float V_j [Bc * d];
         __shared__ float S_ij [Br * Bc];
 
+        // threads' O elements responsibility
+        const int ELEMENTS_PER_THREAD = D/32;
+        float O_reg[ELEMENETS_PER_THREAD] = 0.0f;
+
+        // prev block statistics
+        float m_old = -INFINITY;
+        float d_old = 0.0f;
+
         for (unsigned int offset = 0; offset < d; offset += 32) {
             // assumption: d%32=0
             Q_i[ty_i * d + (tx_i + offset)] = Q[ty_i * d + (tx_i + offset)];
         }
+        __synchronize();
 
         // start for loop for K_j V_j loading
-        for (unsigned int offset = 0; offset < d; offset += (Br * 32) / Bc) {
-            // assumption: d%( (Br * 32) / Bc )=0
-            K_j[ty_j * d + (tx_j + offset)] = K[ty_j * d + (tx_j + offset)];
-            V_j[ty_j * d + (tx_j + offset)] = V[ty_j * d + (tx_j + offset)];
-        }
+        for (unsigned int outerLoop = 0; outerLoop < N; outerLoop += Bc) {
 
-        // calculate S=Q@K^T
-        for(unsigned int offset = 0; offset < d; offset += 32) {
-            float sum = 0.0f;
-            for (unsigned int dotIdx = 0; dotIdx < d; ++dotIdx) {
-                sum += Q_i[ty_i * d + dotIdx] * K_j[(ty_j + offset) * d + dotIdx];
+            for (unsigned int offset = 0; offset < d; offset += (Br * 32) / Bc) {
+                // assumption: d%( (Br * 32) / Bc )=0
+                K_j[ty_j * d + (tx_j + offset)] = K[ty_j * d + (tx_j + offset)];
+                V_j[ty_j * d + (tx_j + offset)] = V[ty_j * d + (tx_j + offset)];
             }
-            S_ij[ty_i * d + (ty_j + offset)] = sum;
+            __synchronize();
+
+            // calculate S=Q@K^T.
+            for(unsigned int offset = 0; offset < Bc; offset += 32) {
+                // assumption: Bc%32 = 0, Bc >> 32
+                float sum = 0.0f;
+                for (unsigned int dotIdx = 0; dotIdx < d; ++dotIdx)
+                    sum += Q_i[ty_i * d + dotIdx] * K_j[(tx_i + offset) * d + dotIdx];  // possible bank conflict at K_j accesses
+                S_ij[ty_i * Bc + (tx_i + offset)] = sum;
+            }
+            __synchronize();
+
+            // thread-level softmax: reduce S_ij into register for online softmax
+            float d_i = 0.0f;
+            float m_i = -INFINITY;
+            for (unsigned int offset = 0; offset < Bc; offset += 32) {
+                // assumption: Bc%32 = 0, Bc >> 32
+                int dataIdx = ty_i * Bc + (tx_i + offset);
+                float val = S_ij[dataIdx];
+
+                float m_prev = m_i;
+                m_i = fmaxf(m_i, val);
+                d_i *= expf(m_prev - m_i);
+                d_i += expf(val - m_i);
+            }
+
+            // warp-level softmax
+            for (unsigned int mirrorIdx = 1; mirrorIdx <= 16; mirrorIdx <<= 1) {
+                float m_j = __shfl_xor_sync(FULL_MASK, m_i, mirrorIdx); // obtain m_j
+                float d_j = __shfl_xor_sync(FULL_MASK, d_i, mirrorIdx); // obtain d_j
+
+                float max = fmaxf(m_i, m_j);    // find new max
+                d_i *= expf(m_i - max);         // rescale old sum
+                d_i += d_j * expf(m_j - max);   // add contribution from current sum
+                m_i = max;
+            }
+
+            // calculate P_ij
+            for (unsigned int offset = 0; offset < Bc; offset += 32) {
+                // assumption: Bc%32 = 0, Bc >> 32
+                int dataIdx = ty_i * Bc + (tx_i + offset);
+                float val = S_ij[dataIdx];
+                S_ij[dataIdx] = expf(val - m_i) / d_i;
+            }
+            __synchronize();
+
+            // now, m_i and d_i are block max and norms
+
+            // global max and norms: calculate new stats (current softmax + prev softmax results)
+            float m_new = fmaxf(m_i, m_old);
+            float d_new = d_old * expf(m_old - m_new) + d_i * expf(m_i - m_new);
+            
+            // matmul PV, load into O
+            #pragma unroll
+            for (unsigned int i = 0; i < ELEMENETS_PER_THREAD; ++i) {
+                // assumption: Bc%32=0, Bc >> 32
+                float sum = 0.0f;
+                for (unsigned int dotIdx = 0; dotIdx < Bc; ++dotIdx) {
+                    sum = S_ij[ty * d + dotIdx] * V_j[dotIdx * d + (tx + (32 * i))];
+                }
+                O_reg[i] *= d_old * (1/d_new) * expf(m_old - m_new) 
+                O_reg[i] += sum * expf(m_i - m_new);
+            }
+
+            // save new globals as prev blocks
+            m_old = m_new;
+            d_old = d_new;
+
+            // advance to the next block
+            K += Bc * d;
+            V += Bc * d;
+
+            // finish for loop
         }
 
-        // thread-level softmax: reduce S_ij into register for online softmax
-        float d_i = 0.0f;
-        float m_i = -INFINITY;
-        for (unsigned int offset = 0; offset < Bc; offset += 32) {
-            // assumption: Bc%32 = 0, Bc >> 32
-            int dataIdx = ty_i * Bc + (tx_i + offset);
-            float val = S_ij[dataIdx];
-
-            float m_prev = m_i;
-            m_i = fmaxf(m_i, val);
-            d_i *= expf(m_prev - m_i);
-            d_i += expf(val - m_i);
+        // write results
+        #pragma unroll
+        for (unsigned int i = 0; i < ELEMENTS_PER_THREAD; ++i) {
+            // assumption: d%32=0, d >> 32
+            O[ty * d + (tx + 32 * i)] = O_reg[i];
         }
-
-        // warp-level softmax
-        for (unsigned int mirrorIdx = 1; mirrorIdx <= 16; mirrorIdx <<= 1) {
-            float m_j = __shfl_xor_sync(FULL_MASK, m_i, mirrorIdx); // obtain m_j
-            float d_j = __shfl_xor_sync(FULL_MASK, d_i, mirrorIdx); // obtain d_j
-
-            float max = fmaxf(m_i, m_j);    // find new max
-            d_i *= expf(m_i - max);         // rescale old sum
-            d_i += d_j * expf(m_j - max);   // add contribution from current sum
-            m_i = max;
-        }
-
-        // calculate P_ij
-        for (unsigned int offset = 0; offset < Bc; offset += 32) {
-            // assumption: Bc%32 = 0, Bc >> 32
-            int dataIdx = ty_i * Bc + (tx_i + offset);
-            float val = S_ij[dataIdx];
-            S_ij[dataIdx] = expf(val - m_i) / d_i;
-        }
-
-        // Matmul PV, load into O + update old stats
-
-        // finish for loop
-
-
 
     }
     
@@ -93,6 +137,24 @@ namespace FlashLab::flashAttn::naive {
     //
     // KERNEL WRAPPERS
     //
-    
+    void launch_flashAttn_fwd_v1(float *K, float *Q, float *V, float *O, const int N, const int d, cudaStream_t stream) {
+        const int Br = 16;
+        const int Bc = 128;
+        dim3 gridDim(CEIL_DIV(N, Br));
+        dim3 blockDim(32 * Br);
+        
+        switch(d) {
+            case(128):
+                flashAttn_fwd_v1<Br, Bc, 128><<<gridDim, blockDim, 0, stream>>>(K, Q, V, O, N, d); break;
+            case(256):
+                flashAttn_fwd_v1<Br, Bc, 256><<<gridDim, blockDim, 0, stream>>>(K, Q, V, O, N, d); break;
+            case(512):
+                flashAttn_fwd_v1<Br, Bc, 512><<<gridDim, blockDim, 0, stream>>>(K, Q, V, O, N, d); break;
+            case(1024):
+                flashAttn_fwd_v1<Br, Bc, 1024><<<gridDim, blockDim, 0, stream>>>(K, Q, V, O, N, d); break;
+            case(2048):
+                flashAttn_fwd_v1<Br, Bc, 2048><<<gridDim, blockDim, 0, stream>>>(K, Q, V, O, N, d); break;
+        }
+    }
 
 }
--- src/03_flashAttn/01_flashAttn_naive/main.cu
+++ src/03_flashAttn/01_flashAttn_naive/main.cu
@@ -5,6 +5,64 @@
 
 
 // -- CPU FUNCTIONS --
+void cpu_onlineSoftmax (float *input, float *output, const int N, const int d, float *time_cpu) {
+  double cpu_start;
+  if (time_cpu != nullptr) cpu_start = FlashLab::get_time_ms();
+  for (unsigned int i = 0; i < N; ++i) {
+
+    // running normalizer and max
+    float m_new = -INFINITY;
+    float m_prev = -INFINITY;
+    float norm = 0.0f;
+    for (unsigned int k = 0; k < d; ++k) {
+      m_new = fmaxf(m_prev, input[i * d + k]);
+
+      norm *= expf(m_prev - m_new);             // adjust to new max
+      norm += expf(input[i * d + k] - m_new);   // add new sum
+      m_prev = m_new;                           // set new max to prev
+    }
+    // don't forget that we cannot 'just hold onto max' until the final iteration. We have to constantly subtract it while doing
+    // our sums to make sure that overflow/underflow does not occur!
+
+
+    // calculate final values
+    for (unsigned int k = 0; k < d; ++k) {
+      output[i * d + k] = expf(input[i * d + k] - m_new) / norm;
+    }
+  }
+  if (time_cpu != nullptr) *time_cpu = static_cast<float>(FlashLab::get_time_ms() - cpu_start);
+}
+
+void cpu_matmul (float *A, float *B, float *output, const int N, const int d, const int common_dim) {
+  for (unsigned int row = 0; row < N; ++row) {
+    for (unsigned int col = 0; col < d; ++col) {
+
+      float sum = 0.0f;
+      for (unsigned int k = 0; k < common_dim; ++k) {
+        sum += A[row * d + k] * B[k * d + col];
+      }
+      output[row * d + col] = sum;
+    }
+  }
+}
+
+void cpu_transpose (float *input, const int N) {
+  for (unsigned int i = 0; i < N; ++i) {
+    for (unsigned int j = 0; j < i; ++j) {
+      float placeholder;
+      placeholder = input[j * d + i];
+      input[j * d + i] = input[i * d + j];
+      input[i * d + j] = placeholder;
+    }
+  }
+}
+
+void cpu_attention (float *K, float *Q, float *V, float *S, float *P, float *O, const int N, const int d) {
+  cpu_transpose(K, N);
+  cpu_matmul(Q, K, S, N, d, d);
+  cpu_onlineSoftmax(S, P, N, N, nullptr);
+  cpu_matmul(P, V, O, N, d, N);
+}
 
 // -- VERIFY FUNCTIONS --
 // softmax can be verified element wise. Use validate func given in ./common/utils.cu
@@ -12,19 +70,33 @@
 int main(void) {
   cudaStream_t stream;
   
-  float *input;
-  float *output;
-  float *output_cpu;
-  float cpu_time;
+  float *K;
+  float *Q;
+  float *V;
+  float *O;
+  float *K_cpu;
+  float *Q_cpu;
+  float *V_cpu;
+  float *S_cpu; // INTERMEDIATE MATRIX (unfortunately materialized in CPU)
+  float *P_cpu; // INTERMEDIATE MATRIX (unfortunately materialized in CPU)
+  float *O_cpu;
+  float cpu_time; // not used rn.
 
   const int N = 16384;
   const int d = 128;
 
 
   // USE UNIFIED MEMORY - INITIALIATONS
-  CUDA_CHECK(cudaMallocManaged(&input, N * d * sizeof(float)));
-  CUDA_CHECK(cudaMallocManaged(&output, N * d * sizeof(float)));
-  output_cpu = (float*)std::malloc(N * d * sizeof(float));
+  CUDA_CHECK(cudaMallocManaged(&K, N * d * sizeof(float)));
+  CUDA_CHECK(cudaMallocManaged(&Q, N * d * sizeof(float)));
+  CUDA_CHECK(cudaMallocManaged(&V, N * d * sizeof(float)));
+  CUDA_CHECK(cudaMallocManaged(&O, N * d * sizeof(float)));
+  K_cpu = (float*)std::malloc(N * d * sizeof(float));
+  Q_cpu = (float*)std::malloc(N * d * sizeof(float));
+  V_cpu = (float*)std::malloc(N * d * sizeof(float));
+  S_cpu = (float*)std::malloc(N * d * sizeof(float));
+  P_cpu = (float*)std::malloc(N * d * sizeof(float));
+  O_cpu = (float*)std::malloc(N * d * sizeof(float));
   CUDA_CHECK(cudaStreamCreate(&stream));
 
   // -- BENCHMARK STATISTICS --
@@ -40,16 +112,28 @@ int main(void) {
   //FlashLab::Benchmark::benchmark_kernel(launch_kernel, stream, bytes_moved, cpu_time, num_repeats, num_warmups, true);
 
   // -- VERIFY KERNEL RUN --
-  FlashLab::initMatrix(input, N, d);                                  // Init Matrix
-  cpu_onlineSoftmax(input, output_cpu, N, d, nullptr);                // Store CPU Result
-  FlashLab::Softmax::launch_softmax_v2(type, BN, input, output, N, d, stream);  // Store GPU Result
+  FlashLab::initMatrix(K, N, d);
+  FlashLab::initMatrix(Q, N, d);
+  FlashLab::initMatrix(V, N, d);
+  FlashLab::copyMatrix(K, K_cpu, N, d);
+  FlashLab::copyMatrix(Q, Q_cpu, N, d);
+  FlashLab::copyMatrix(V, V_cpu, N, d);
+  cpu_attention(K_cpu, Q_cpu, V_cpu, S_cpu, P_cpu, O_cpu);
+  FlashLab::flashAttn::naive::launch_flashAttn_fwd_v1(K, Q, V, O, N, d, stream);
   CUDA_CHECK(cudaDeviceSynchronize());
-  if (FlashLab::validate(output, output_cpu, N * d)) std::printf("Succes!\n");  // VERIFY KERNEL
+  if (FlashLab::validate(O, O_cpu, N * d)) std::printf("Succes!\n");  // VERIFY KERNEL
 
   // FREE MEMORY ALLOCATION
-  CUDA_CHECK(cudaFree(input));
-  CUDA_CHECK(cudaFree(output));
-  std::free(output_cpu);
+  CUDA_CHECK(cudaFree(K));
+  CUDA_CHECK(cudaFree(Q));
+  CUDA_CHECK(cudaFree(V));
+  CUDA_CHECK(cudaFree(O));
+  std::free(K_cpu);
+  std::free(Q_cpu);
+  std::free(V_cpu);
+  std::free(S_cpu);
+  std::free(P_cpu);
+  std::free(O_cpu);
 
   // DESTROY STREAM
   CUDA_CHECK(cudaStreamDestroy(stream));
